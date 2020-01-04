0fb3836747f18002544966e780ef05a252e2faaf
Souman Paul <souman1729@gmail.com>
2020-01-05 01:21:58 +0530

    commit

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 const cookieParser = require('cookie-parser')
 var passport = require('passport');
 const connectDB = require('./config/db');
-const path = require('path');
 
 
 // Route files
@@ -51,14 +50,14 @@ app.use('/api/v1/comments', commentRouter);
 
 
 // Serve static assets in production
-if(process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+// if(process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+// })
+// }
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
